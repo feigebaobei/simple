1495502de26209b78b39503328acaaa43d6c0bd7
feigebaobei <18515195415@163.com>
2022-12-05 11:51:27 +0800

    feat(redux-simple): init

---
+++ packages/redux-simple/__tests__/redux-simple.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const reduxSimple = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(reduxSimple(), 'Hello from reduxSimple');
+console.info("reduxSimple tests passed");
